1cd67f7ed3dafbac6ee9c7c87f604a1a628f6284
Lefebvre genius <lefebvregenius@gmail.com>
2026-06-08 20:07:11 +0100

    modification index et css,js

--- js/script.js
+++ js/script.js
@@ -632,3 +632,16 @@ overlay?.addEventListener("click", () => {
   });
 
 })();
+const images = document.querySelectorAll('.hero-bg');
+
+let current = 0;
+
+setInterval(() => {
+
+    images[current].classList.remove('active');
+
+    current = (current + 1) % images.length;
+
+    images[current].classList.add('active');
+
+}, 5000);
